22b5e972c7b120966b69a151bfcab5ca3700d5f1
skyscreen <siyanzhao30@hotmail.com>
2017-09-26 23:28:16 +0800

    simple hello world for log4j

---
+++ devtest/Readme.md
@@ -0,0 +1,3 @@
+
+Step 1: mvn
+Step 2: mvn -e exec:java -Dexec.mainClass="com.hydsoft.HelloExample"
---
+++ devtest/src/main/java/com/hydsoft/HelloExample.java
@@ -0,0 +1,33 @@
+package com.hydsoft;
+
+import org.apache.log4j.Logger;
+
+public class HelloExample{
+
+	final static Logger logger = Logger.getLogger(HelloExample.class);
+
+	public static void main(String[] args) {
+
+		HelloExample obj = new HelloExample();
+		obj.runMe("hydsoft");
+
+	}
+
+	private void runMe(String parameter){
+
+		if(logger.isDebugEnabled()){
+			logger.debug("This is debug : " + parameter);
+		}
+
+		if(logger.isInfoEnabled()){
+			logger.info("This is info : " + parameter);
+		}
+
+		logger.warn("This is warn : " + parameter);
+		logger.error("This is error : " + parameter);
+		logger.fatal("This is fatal : " + parameter);
+
+		System.out.println("This is log4j running @" + parameter);
+	}
+
+}
